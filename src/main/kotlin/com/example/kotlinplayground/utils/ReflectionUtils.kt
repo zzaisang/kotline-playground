fbb47af1d8cdfe3693c49e3d6cd774266e9efdf1
zzai_sang <zzai_sang@naver.com>
2023-04-12 09:34:29 +0900

    Change ReflectionUtils
    기존에 Utils 을 선언 후 사용해야하는 방식에서 함수만 정의되도록 변경

--- src/main/kotlin/com/example/kotlinplayground/utils/ReflectionUtils.kt
+++ src/main/kotlin/com/example/kotlinplayground/utils/ReflectionUtils.kt
@@ -1,22 +1,17 @@
 package com.example.kotlinplayground.utils
 
-import kotlin.reflect.full.createInstance
-import kotlin.reflect.full.memberProperties
+class ReflectionUtils
 
-class ReflectionUtils {
-
-    inline fun <reified T : Any, reified R : Any> convertObject(source: T): R {
-        val target = R::class.java.getConstructor().newInstance()
-        val sourceFields = T::class.java.declaredFields.associateBy { it.name }
-        R::class.java.declaredFields.forEach { targetField ->
-            val sourceField = sourceFields[targetField.name]
-            if (sourceField != null) {
-                sourceField.isAccessible = true
-                targetField.isAccessible = true
-                targetField.set(target, sourceField.get(source))
-            }
+inline fun <reified T : Any, reified R : Any> convertObject(source: T): R {
+    val target = R::class.java.getConstructor().newInstance()
+    val sourceFields = T::class.java.declaredFields.associateBy { it.name }
+    R::class.java.declaredFields.forEach { targetField ->
+        val sourceField = sourceFields[targetField.name]
+        if (sourceField != null) {
+            sourceField.isAccessible = true
+            targetField.isAccessible = true
+            targetField.set(target, sourceField.get(source))
         }
-        return target
     }
-
+    return target
 }

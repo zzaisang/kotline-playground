804723fd7539aac85d3dd860c04e97cd844b6b8a
zzai_sang <zzai_sang@naver.com>
2023-04-12 09:24:56 +0900

    Change Reflection Target setting
    기존 clazz 에서 getDelcaredConsturctor 방식에서 R::class.java.getConstructor() 방식으로 변경

--- src/main/kotlin/com/example/kotlinplayground/utils/ReflectionUtils.kt
+++ src/main/kotlin/com/example/kotlinplayground/utils/ReflectionUtils.kt
@@ -5,22 +5,8 @@ import kotlin.reflect.full.memberProperties
 
 class ReflectionUtils {
 
-    inline fun <reified T : Any> applyFieldsFromMap(map: Map<String, Any?>, clazz: Class<T>): T {
-        val instance = clazz.getDeclaredConstructor().newInstance()
-        clazz.declaredFields.forEach { field ->
-            if (map.containsKey(field.name)) {
-                val value = map[field.name]
-                if (value != null && field.type.isInstance(value)) {
-                    field.isAccessible = true
-                    field.set(instance, value)
-                }
-            }
-        }
-        return instance
-    }
-
     inline fun <reified T : Any, reified R : Any> convertObject(source: T): R {
-        val target = R::class.createInstance()
+        val target = R::class.java.getConstructor().newInstance()
         val sourceFields = T::class.java.declaredFields.associateBy { it.name }
         R::class.java.declaredFields.forEach { targetField ->
             val sourceField = sourceFields[targetField.name]
@@ -32,6 +18,5 @@ class ReflectionUtils {
         }
         return target
     }
-
 
 }

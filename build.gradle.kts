3864a43ab04d371d3bcf84c046d99cc344d0c400
zzai_sang <zzai_sang@naver.com>
2023-04-12 09:26:51 +0900

    update kotest dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     testImplementation("org.modelmapper:modelmapper:3.1.1")
 }

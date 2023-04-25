2c89ab1f55b11dd90e322b5af320b66ae1ca6e62
zzai_sang <zzai_sang@naver.com>
2023-04-25 09:14:32 +0900

    Remove Unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.modelmapper:modelmapper:3.1.1")
 
     implementation("org.jetbrains.kotlin:kotlin-noarg")

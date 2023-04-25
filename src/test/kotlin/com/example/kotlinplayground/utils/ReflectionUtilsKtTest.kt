a2df92c50fb0feffbafe794ecf947e2c33db551c
zzai_sang <zzai_sang@naver.com>
2023-04-25 09:36:44 +0900

    Add ReflectionUtilsTest with elapsedTime

--- src/test/kotlin/com/example/kotlinplayground/utils/ReflectionUtilsKtTest.kt
+++ src/test/kotlin/com/example/kotlinplayground/utils/ReflectionUtilsKtTest.kt
@@ -5,17 +5,21 @@ import com.example.kotlinplayground.common.utils.convertObject
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import kotlin.system.measureTimeMillis
 
 class ReflectionUtilsKtTest : DescribeSpec({
 
     describe("Reflection 할 대상의 DTO 에") {
         context("@NoArg 가 존재하는 경우") {
             val beforeDto = BeforeDto("data1")
-            val afterDto = convertObject<BeforeDto, AfterDto>(beforeDto)
-            it("성공적으로 변경이 된다.") {
-                afterDto.data1 shouldBe beforeDto.data1
-            }
+            measureTimeMillis {
+                val afterDto = convertObject<BeforeDto, AfterDto>(beforeDto)
+                it("성공적으로 변경이 된다.") {
+                    afterDto.data1 shouldBe beforeDto.data1
+                }
+            }.let { println("Reflection 소요시간 : $it ms") }
         }
+
         context("@NoArg 가 존재하지 않는 경우") {
             val beforeDto = AfterDto("data1")
             it("NoSuchMethod Exception 이 발생한다.") {
